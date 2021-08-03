f462a2c34b79f57f28281c07d642f3ea1a008990
SJ <sotsu@adachi-printing.co.jp>
2021-08-03 16:37:13 +0900

    微調整

--- src/components/ClockDigital.tsx
+++ src/components/ClockDigital.tsx
@@ -1,14 +1,24 @@
+import { useState, useEffect } from 'react';
 import { FC } from 'react';
 
 import { StyledClock } from './style';
 import useTime from './useTime';;
 
 export const Clock: FC<{ size?: 'small' | 'large' }> = ({ size = 'large' }) => {
+    const [{ hours, minutes, seconds }, setState] = useState({ hours: '00', minutes: '00', seconds: '00' });
     const [time] = useTime();
+
+    useEffect(() => {
+        setState({
+            hours: `0${time.getHours()}`.slice(-2),
+            minutes: `0${time.getMinutes()}`.slice(-2),
+            seconds: `0${time.getSeconds()}`.slice(-2),
+        })
+    }, [time]);
 
     return (
         <StyledClock size={size}>
-            {`0${time.getHours()}`.slice(-2)}:{`0${time.getMinutes()}`.slice(-2)}:{`0${time.getSeconds()}`.slice(-2)}
+            {hours}:{minutes}:{seconds}
         </StyledClock>
     )
 }
